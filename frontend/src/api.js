7635c4eeeae1c4e7ffafb432dc01533c1bd743c4
Aayush Kholay <kholay0101@gmail.com>
2026-04-08 12:45:50 +0530

    Update API endpoint to production URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,7 +5,7 @@ export const processResumes = async (files, jd, skills) => {
   formData.append("jd", jd);
   formData.append("jd_skills", skills);
 
-  const res = await fetch("http://127.0.0.1:8000/process_resume/", {
+  const res = await fetch("https://candidai-version-1.onrender.com/process_resume/", {
     method: "POST",
     body: formData,
   });
