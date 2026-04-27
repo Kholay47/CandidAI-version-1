50ac2ad9e5a16fea67e76d1a637fb555492ebd1d
Aayush Kholay <kholay0101@gmail.com>
2026-04-27 23:17:34 +0530

    Update Vite config to specify allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+
   build: {
     outDir: "dist",
   },
 
   preview: {
-    host: "0.0.0.0",                // ✅ required for Render
-    port: process.env.PORT,         // ✅ bind to Render port
-    allowedHosts: "all"             // ✅ allow Render domain
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    allowedHosts: [
+      "candidai-version-1-1.onrender.com"
+    ]
   }
 })
